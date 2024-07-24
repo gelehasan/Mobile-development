35c1156afe2a436ea67efd499bcc61c7f8029d36
Gele hasan <gellehussen@gmail.com>
2024-07-24 13:43:32 +0200

    testing json file

---
+++ components/Json/json.js
@@ -0,0 +1,15 @@
+import JSONDATA from "../../../test.json";
+import { View, Text } from "react-native";
+
+
+export default function JsonTest (){
+
+
+
+    return(
+        <View>
+            <Text> Hello {JsonTest.name}</Text>
+        </View>
+    
+)
+}
